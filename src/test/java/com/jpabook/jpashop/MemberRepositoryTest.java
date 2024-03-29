bfef24c6935d4db5c070d9a982741907a144d093
Seung Joo <psjoo3515@gmail.com>
2024-03-29 16:48:54 +0900

    Refactoring

--- src/test/java/com/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/com/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,34 +1,36 @@
-package com.jpabook.jpashop;
-
-
-import com.jpabook.jpashop.domain.Member;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-class MemberRepositoryTest {
-    @Autowired MemberRepository memberRepository;
-    
-    @Test
-    @Transactional
-    @Rollback(false) //롤백 제외 -> 커밋 실행함
-    public void testMember() throws Exception{
-        // given
-        Member member = new Member();
-        member.setName("memberA");
-        
-        // when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-        
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
-        Assertions.assertThat(findMember).isEqualTo(member);
-        System.out.println("findMember == member:" + (findMember == member));
-    }
-}
+//package com.jpabook.jpashop;
+//
+//
+//import com.jpabook.jpashop.domain.Member;
+//import com.jpabook.jpashop.service.MemberService;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//class MemberRepositoryTest {
+//    @Autowired
+//    MemberService memberRepository;
+//
+//    @Test
+//    @Transactional
+//    @Rollback(false) //롤백 제외 -> 커밋 실행함
+//    public void testMember() throws Exception{
+//        // given
+//        Member member = new Member();
+//        member.setName("memberA");
+//
+//        // when
+//        Long savedId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(savedId);
+//
+//        //then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+//        Assertions.assertThat(findMember).isEqualTo(member);
+//        System.out.println("findMember == member:" + (findMember == member));
+//    }
+//}
