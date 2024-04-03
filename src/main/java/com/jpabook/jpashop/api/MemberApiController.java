2e6e96a75086037033f26a9eb383fd42e96e1a93
Seung Joo <psjoo3515@gmail.com>
2024-04-03 14:22:49 +0900

    PUT Membership registration v1, v2 Test

--- src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -4,12 +4,10 @@ import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,13 +27,39 @@ public class MemberApiController {
     // V2
     // API 스펙 바뀌지 않음
     @PostMapping("/v2/members")
-    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+    public CreateMemberResponse saveMemberV2(
+            @RequestBody
+            @Valid CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.name);
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    @PutMapping("/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+
 
     // V1
     @Data
