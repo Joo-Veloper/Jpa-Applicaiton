2ff9ab3891d6bcd5d3ff024c02f34a325ea19970
Seung Joo <psjoo3515@gmail.com>
2024-04-09 16:03:25 +0900

    Optimized collection lookup

--- src/main/java/com/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/com/jpabook/jpashop/api/OrderApiController.java
@@ -74,9 +74,12 @@ public class OrderApiController {
     @GetMapping("/api/v4/orders")
     public List<OrderQueryDto> ordersV4() {
         return orderQueryRepository.findOrderQueryDtos();
-
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
 
 
     @Getter
